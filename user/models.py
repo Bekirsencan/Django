e588194c20b97cf56441c820ddc6b5892356ba13
Bekir <PmyoViki@gmail.com>
2021-12-08 16:33:17 +0300

    Database connected % User model created

--- user/models.py
+++ user/models.py
@@ -1,4 +1,10 @@
 from django.db import models
 
-class User(models.Model):
 
+class User(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    user_name = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    db_table = 'user'
